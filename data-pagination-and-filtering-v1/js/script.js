c8cc057653b14a7e00f047b39dba9328c96015c5
arielle1127 <81972975+arielle1127@users.noreply.github.com>
2021-04-26 20:18:20 +0400

    template literal

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -3,13 +3,68 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+/*BIG GOAL: Display a directory of student cards on a page
+         9 students on the page at one time
+         Add buttons that navigate to the next 9 students
 
+1st Goal: Display a page of 9 students
+         Use a function 
+         name this function showPage. 
+         two parameters: list, and page.
+           list parameter will represent the array of student data 
+           page parameter will be the page number that we want to display.
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
+
+
+
+function showPage(list, page) {
+   const itemsperPage = 9
+   const startIndex = (page * itemsperPage) - itemsperPage;     
+   const endIndex = page * itemsperPage; 
+   let studentList = document.querySelector('.student-list'); 
+   studentList.innerHTML = ''; 
+      for (let i = 0; i < list.length; i++) 
+         
+        if (list[i] < endIndex && list[i] >= startIndex) {   
+          let  studentItem = `<li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+            <h3>Ethel Dean</h3>
+            <span class="email">ethel.dean@example.com</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined 12-15-2005</span>
+          </div>
+        </li>`;
+         
+  
+
+
+
+///Create new elements needed to display the student information
+//Use bracket notation to access the student object at that index
+// and dot notation to access the specific properties of that student object. 
+       
+
+// insert the above elements to the studentList variable
+//insertAdjacentHTML method and beforeend option works well for this.
+
+
+       
+         
+                                             
+   }
+ console.log(page);
+ console.log(list);
+ console.log(studentList);
+ console.log(studentItem);
+}
+
+showPage(data, 1);
+
+
+
 
 
 
